2bd2b956ff48cfbbd149c7bfdd2dc22e52db45a8
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-09-25 07:17:04 +0200

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+// import {
+//   trees as parkTrees,
+//   wildlife as parkWildlife,
+// } from "./parks/RockyMountain";
+import * as RMFucntions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
+
+howManyParks();
+
+console.log(parkTrees);
+parkWildlife();
+
+export default ColoradoStateParks;
